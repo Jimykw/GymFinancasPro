ac7b24aa64fec25150ea7e796e3268d5d650a472
Jimy.dev <kwjimy@gmail.com>
2026-04-06 16:23:55 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/GymFinancas-Pro/',
+  base: '/GymFinancasPro/', // Corrigido: sem hífen, igual ao nome do repositório no GitHub!
   plugins: [
     react(),
     tailwindcss(),
